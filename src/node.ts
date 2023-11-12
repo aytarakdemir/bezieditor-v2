78fd3f3ecb44d6ba882423151f12ed4c5dfc60c5
aytarakdemir <aytar.akdemir@outlook.com>
2023-11-12 14:29:57 +0300

    Impl node class

---
+++ src/node.ts
@@ -0,0 +1,55 @@
+
+export enum NodeType {
+    regular,
+    controlStart,
+    controlEnd,
+}
+
+export class Node {
+    constructor(private x: number, private y: number, private type: NodeType = NodeType.regular) {
+    }
+
+    // Move the node to a new position
+    update(newX: number, newY: number): void {
+        this.x = newX;
+        this.y = newY;
+    }
+
+    getCoords(): { x: number, y: number } {
+        return { x: this.x, y: this.y };
+    }
+
+    getType() : NodeType {
+        return this.type;
+    }
+
+    // Render the node on the canvas as a point
+    draw(ctx: CanvasRenderingContext2D): void {
+        switch (this.type) {
+            case NodeType.regular:
+                ctx.fillStyle = "yellow";
+                ctx.lineWidth = 1;
+                ctx.beginPath();
+                ctx.rect(this.x - 5, this.y - 5, 10, 10);
+                ctx.fill();
+                ctx.stroke();        
+                break;
+            case NodeType.controlStart:
+                ctx.lineWidth = 1;
+                ctx.fillStyle = "red";
+                ctx.beginPath();
+                ctx.ellipse(this.x, this.y, 5, 5, Math.PI / 4, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.stroke();        
+                break;
+            case NodeType.controlEnd:
+                ctx.lineWidth = 1;
+                ctx.fillStyle = "blue";
+                ctx.beginPath();
+                ctx.ellipse(this.x , this.y , 5, 5, Math.PI / 4, 0, 2 * Math.PI);
+                ctx.fill();
+                ctx.stroke();        
+                break;
+        }
+    }
+}
--- src/scene.ts
+++ src/scene.ts
@@ -1,4 +1,5 @@
 
+import {Node, NodeType} from './node';
 
 export class Scene {
 
@@ -8,6 +9,8 @@ export class Scene {
     private pivotX: number = 0;
     private pivotY: number = 0;
 
+    private isEditing = false;
+
 
     constructor() {
         this.canvas = document.getElementById('canvas') as HTMLCanvasElement;
@@ -15,29 +18,85 @@ export class Scene {
         window.addEventListener('resize', this.resizeCanvas);
         this.ctx = (this.canvas as HTMLCanvasElement).getContext('2d')!;
         this.animate();
+        this.initControlMenuEvents();
     }
 
-    private resizeCanvas= (): void => {
+    private resizeCanvas = (): void => {
         this.canvas.width = window.innerWidth;
         this.canvas.height = window.innerHeight;
     }
 
+    private initControlMenuEvents() {
+        const checkBox = document.getElementById('edit-state') as HTMLInputElement;
+        const button = document.getElementById('btn-input') as HTMLButtonElement;
+        const textarea = document.getElementById('bezier-input') as HTMLTextAreaElement;
+        const outputButton = document.getElementById('btn-output') as HTMLButtonElement;
+        const outputParagraph = document.getElementById('output') as HTMLParagraphElement;
 
-    
+        checkBox.checked = this.isEditing;
+        checkBox.addEventListener('change', () => {
+            if (checkBox.checked) {
+                this.isEditing = true;
+            } else {
+                this.isEditing = false;
+            }
+        });
+        button.addEventListener('click', () => {
+            try {
+                const inputValue = JSON.parse(textarea.value) as number[][];
+                if (this.isValidNumberArrayArray(inputValue)) {
+                    this.processInput(inputValue);
+                } else {
+                    alert('Invalid input format. Expected an array of arrays of numbers.');
+                    console.error('Invalid input format. Expected an array of arrays of numbers.');
+                }
+            } catch (error) {
+                alert('Invalid input format. Expected an array of arrays of numbers.');
+                console.error('Invalid JSON format:', error);
+            }
+        });
+        // outputButton.addEventListener('click', () => {
+        //     const formattedArrayString = '[' + this.bezierCoordinates.map(subArray => 
+        //         '\n[' + subArray.join(', ') + ']'
+        //     ).join(',') + '\n]';            
+        //     outputParagraph.textContent = formattedArrayString;
+        // });
+    }
+
+    private processInput(inputValue: number[][]) {
+        console.log('Validated input:', inputValue);
+
+        // this.bezierCoordinates=inputValue;
+    }
+
+    private isValidNumberArrayArray(input: any): input is number[][] {
+        return Array.isArray(input) &&
+            input.every(subArray =>
+                Array.isArray(subArray) &&
+                subArray.every(item => typeof item === 'number')
+            );
+    }
+
     public draw(): void {
+        const node1 = new Node(5, 10, NodeType.regular);
+        node1.draw(this.ctx);
+        const node2 = new Node(5, 10, NodeType.controlStart);
+        node2.draw(this.ctx);
+        const node3 = new Node(5, 10, NodeType.controlEnd);
+        node3.draw(this.ctx);
     }
 
     public setPivot(x: number, y: number): void {
         this.pivotX = x;
-        this.pivotY = y;    
+        this.pivotY = y;
     }
 
     public setCoordinates(coords: number[][]) {
         console.log(coords);
     }
 
-    
-    private animate():void {
+
+    private animate(): void {
         this.ctx.clearRect(0, 0, innerWidth, innerHeight);
         this.draw();
         requestAnimationFrame(this.animate.bind(this));
